08bb19bed767d96a16ae09b8c47d43d0c355171d
Nimesha Edirisinghe <nimesha.edirisinghe@aspl.com>
2024-01-29 12:59:30 +0530

    add comments

--- src/api/apiInstances/apiClient.ts
+++ src/api/apiInstances/apiClient.ts
@@ -1,21 +1,24 @@
 const baseURL = 'https://randomuser.me/api/';
 
+// Asynchronous function for making API requests
 const apiClient = async (
   url: string,
   method: string,
-  data?: Record<string, unknown>,
+  body?: Record<string, unknown>,
   queryParams?: Record<string, string>
 ) => {
   const headers = {
     'Content-Type': 'application/json',
   };
 
+  // Options for the fetch function
   const options: RequestInit = {
     method,
     headers,
-    body: data ? JSON.stringify(data) : undefined,
+    body: body ? JSON.stringify(body) : undefined,
   };
 
+  // Constructing the full URL with base URL, endpoint, and query parameters
   let fullUrl = `${baseURL}${url}`;
   if (queryParams) {
     const queryString = new URLSearchParams(queryParams).toString();
@@ -23,12 +26,12 @@ const apiClient = async (
   }
 
   try {
+    // Making the API request using the fetch function
     const response = await fetch(fullUrl, options);
 
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
     }
-
     return await response.json();
   } catch (error) {
     throw error;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,34 +2,46 @@ import { userApi } from '../api';
 import { FormDataI, UserResponseI } from '../types/response/user';
 import { DATA_LIMIT } from '../utils/constants';
 
+
 export class UserService {
+  // Initializing a variable to store user response data
   private userResponse: UserResponseI | null = null;
 
+  // Async function to fetch user data
   async getUser(): Promise<void> {
     try {
+      // Setting query parameters for the API request
       const queryParams = {
         results: DATA_LIMIT,
       };
+
+      // Making an API call to get user data
       const response: UserResponseI = await userApi.getUser(
         undefined,
         queryParams
       );
-      const userData: UserResponseI = response;
 
+      // Storing the received user data in the class variable
+      const userData: UserResponseI = response;
       this.userResponse = userData;
     } catch (error) {
+      // Handling errors during the API call
       console.error('Error fetching user:', error);
       throw error;
     }
   }
 
+  // Function to update user data
   updateUser(updatedUserData: FormDataI): void {
     try {
+      // Destructuring properties from the updated user data
       const { address, contactNumber, email, firstName, lastName } =
         updatedUserData;
 
+      // Storing the original user response object
       const originalObject = this.userResponse;
 
+      // Updating the user data based on email match
       const updatedResults = originalObject?.results?.map((user) =>
         user.email === email
           ? {
@@ -48,18 +60,22 @@ export class UserService {
           : user
       );
 
+      // Creating a new object with updated user data
       const updatedObject = {
         ...originalObject,
         results: updatedResults,
       };
 
+      // Storing the updated user response object
       this.userResponse = updatedObject;
     } catch (error) {
+      // Handling errors during the update process
       console.error('Error updating user:', error);
       throw error;
     }
   }
 
+  // Function to get the stored user response object
   getUserResponse(): UserResponseI | null {
     return this.userResponse;
   }
